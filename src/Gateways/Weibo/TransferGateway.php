3de46ec9d7b07a8f2de1a6174c35f59dac3d7152
braum <35051868+braumye@users.noreply.github.com>
2021-07-27 10:13:49 +0800

    Add supports for transfer of weibo (#2)
    * Create TransferConfirmGateway.php
    
    * Create TransferGateway.php

---
+++ src/Gateways/Weibo/TransferGateway.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Braumye\Pay\Gateways\Weibo;
+
+use Braumye\Pay\Gateways\Weibo\Support;
+use Yansongda\Pay\Contracts\GatewayInterface;
+use Yansongda\Pay\Events;
+use Yansongda\Pay\Exceptions\GatewayException;
+use Yansongda\Pay\Exceptions\InvalidConfigException;
+use Yansongda\Pay\Exceptions\InvalidSignException;
+use Yansongda\Supports\Collection;
+
+class TransferGateway implements GatewayInterface
+{
+    /**
+     * Pay an order.
+     *
+     * @param string $endpoint
+     *
+     * @throws GatewayException
+     * @throws InvalidConfigException
+     * @throws InvalidSignException
+     */
+    public function pay($endpoint, array $payload): Collection
+    {
+        $payload['sign_type'] = $payload['sign_type'] ?? 'rsa';
+        $payload['sign'] = Support::generateSign($payload);
+
+        $endpoint = $endpoint.'transfer/create';
+
+        Events::dispatch(new Events\PayStarted('Weibo', 'Transfer', $endpoint, $payload));
+
+        return Support::requestApi($endpoint, $payload);
+    }
+
+    /**
+     * Find.
+     *
+     * @param $order
+     */
+    public function find($order): array
+    {
+        return [
+            'endpoint' => 'transfer/query',
+            'transfer_id' => $order['transfer_id'] ?? $order,
+        ];
+    }
+}
